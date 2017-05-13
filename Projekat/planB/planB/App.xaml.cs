e644278c41929cf0734095869a9dda159a33bc93
AjlaBecic <abecic1@etf.unsa.ba>
2017-05-13 22:12:57 +0200

    Forme za: profil, novosti, prikaz pjesme, postavke, unos obaveze i korisnicka kontrola

--- Projekat/planB/planB/App.xaml.cs
+++ Projekat/planB/planB/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using planB.View;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -72,7 +73,7 @@ namespace planB
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(UnosObavezeForm), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
